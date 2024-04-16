680c58dd1b567388618d3e2b097f1a3c57b177a9
PlutoA713N <mallachandu99@gmail.com>
2024-04-16 15:40:12 +0530

    Update state with mobx

--- src/Components/GetResults.jsx
+++ src/Components/GetResults.jsx
@@ -1,44 +1,49 @@
-import { useEffect, useState } from "react"
+import React, { useEffect } from "react";
+import { observer } from "mobx-react";
+import { autorun, observable, runInAction } from "mobx";
+import { action } from "mobx";
 
-const GetResults= ({}) => {
+const results = observable([]);
 
+const GetResults = observer(() => {
 
-    const [results, setResults] = useState([])
-   
-    useEffect(() => {  
-        const fetchData = async() => {
-            try{
-            const response = await fetch('http://localhost:1337/quiz/results')
-            if( !response.ok ){
-                throw new Error('Network response was not ok');
-            }
+  const fetchData = action(async () => {
+    try {
+      const response = await fetch("http://localhost:1337/quiz/results");
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
 
-            const data = await response.json()
-            setResults( data )
+      const data = await response.json();
+    
+      runInAction(() => {
+        results.replace(data);
+      });
 
-            }catch(error) {
-                console.error("Fetch error:", error);
-            }
-        }
+    } catch (error) {
+      console.error("Fetch error:", error);
+    }
+  });
 
-        fetchData()
+  useEffect(() => {
+    const disposer = autorun(() => {
+      fetchData();
+    });
 
-    }, [])
+    return () => disposer();
+  }, []);
 
-    return(
-        <>
-            {
-                results.map((result, index) => (
-                    <div style={{border: '2px solid black'}} key={index}>
-                        <p>userId: {result.userId}</p>
-                        <p>quizId: {result.quizId}</p>
-                        <p>score: {result.score}</p>
-                    </div>
-                ))
-            }
-        </>
-    )
-}
+  return (
+    <>
+      {results.map((result, index) => (
+        <div style={{ border: "2px solid black" }} key={index}>
+          <p>userId: {result.userId}</p>
+          <p>quizId: {result.quizId}</p>
+          <p>score: {result.score}</p>
+        </div>
+      ))}
+    </>
+  );
+});
 
-
-export default GetResults
+export default GetResults;
