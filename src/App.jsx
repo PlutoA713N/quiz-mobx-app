21c48c11524891db24c85169ee3e19c50070614e
PlutoA713N <mallachandu99@gmail.com>
2024-04-22 18:09:34 +0530

    Add Routes && import Authentication

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,47 @@
-import GetQuiz from './Components/GetQuiz'
-import GetResults from './Components/GetResults'
-import CreateQuiz from './Components/CreateQuiz'
+import React, { useState } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
+import CreateQuiz from "./Components/CreateQuiz";
+import GetQuiz from "./Components/GetQuiz";
+import GetResults from "./Components/GetResults";
+import LoginButton from "./Components/LoginButton";
+import LogoutButton from "./Components/LogoutButton";
+import Profile from "./Components/Profile";
+import { useAuth0 } from "@auth0/auth0-react";
 
-function App() {
+
+const App = () => {
+
+  const [showResults, setShowResults] = useState(false)
+
+  const { isLoading, error, isAuthenticated } = useAuth0();
+  if (error) {
+    return <p>Authentication Error</p>;
+  }
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
 
   return (
-    <>
-     <GetQuiz id={6} />
-    <CreateQuiz />
-    <GetResults />
-    </>
-  )
+    <Router>
+      <div>
+       <h1>Welcome to the App</h1>
+        <h2>Auth0 Login</h2>
+        <LoginButton />
+        <LogoutButton />
+        <Profile />
+        <button onClick={() => setShowResults(!showResults)}>Show Results</button>
+        { showResults && isAuthenticated && <GetResults />}
+      </div>
+
+      <Routes>
+ 
+        <Route path="/get-quiz/:id" element={<GetQuiz />} />
+      
+      </Routes>
+
+    </Router>
+  );
 }
 
-export default App
+export default App;
