9a3c652650f5c341d57da4c51fa35a4846c927f9
PlutoA713N <mallachandu99@gmail.com>
2024-04-24 12:03:03 +0530

    Add backticks && Wrap with Router

--- src/main.jsx
+++ src/main.jsx
@@ -8,20 +8,20 @@ import GetQuiz from './Components/GetQuiz';
 const root = createRoot(document.getElementById('root'));
 
 root.render(
+  <Router>
   <Auth0Provider
     domain="dev-3e5w3b8uhxdlo41m.us.auth0.com"
     clientId="kgi53MIzOzLuyCAgd99t1Bel5rQehLeW"
     authorizationParams={{
-      redirect_uri: window.location.origin,
+      redirect_uri: `${window.location.origin}`,
       audience: "https://dev-3e5w3b8uhxdlo41m.us.auth0.com/api/v2/",
       scope: "read:current_user update:current_user_metadata"
     }}
   >
-    <Router>
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/quiz/:id" element={<GetQuiz />} />
       </Routes>
-    </Router>
   </Auth0Provider>
+  </Router>
 );
